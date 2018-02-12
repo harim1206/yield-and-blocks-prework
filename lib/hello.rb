92143c51ae9c0a179fd4da98bcb186eab4429e13
Harim Choi <harim1206@gmail.com>
2018-02-12 18:56:08 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -11,9 +11,9 @@ end
 
 # call your method here!
 
-array = ["Tim", "Tom", "Jim"]
+arr = ["Tim", "Tom", "Jim"]
 
-hello_t(array) do |name|
+hello_t(arr) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
